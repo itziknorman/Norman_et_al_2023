efab0a4dad4a0bac1d99bfce69fa923f0bc80000
itziknorman <59057794+itziknorman@users.noreply.github.com>
2024-01-08 10:24:36 -0800

    Add files via upload

---
+++ general_function_github/set_figure_colors_PP.m
@@ -0,0 +1,23 @@
+%% set_figure_colors:
+COLOR=struct;
+COLOR.red=[0.8 0.2 0.2];
+COLOR.lightred=[0.8 0.5 0.5];
+COLOR.green=[0.2 0.8 0.2];
+COLOR.lightgreen=[0.2 0.8 0.5];
+COLOR.blue=[0.0706    0.2    0.6]; 
+COLOR.gray=[0.3 0.3 0.3];
+COLOR.yellow=[1,1,0];
+COLOR.orange=[1,0.7,0.1];
+COLOR.lightblue=[0.4 0.6 0.8];  
+COLOR.purple=[1 0 1];  
+COLOR.black=[0 0 0];   
+COLOR.lightgray=[0.8 0.8 0.8];
+COLOR.redsharp=[1 0 0];
+COLOR.bordo=[0.4    0.0    0.05]; 
+
+COLOR.rest1 = [0.05    0.05    0.05];
+COLOR.movie2 = [0.9490    0.2196    0.0784];
+COLOR.rest2 = [0.0706    0.2    0.6];
+COLOR.movie1 = [1,0.7,0.1];
+COLOR.memorytest = [0.4745    0.6431    0.2275];
+
