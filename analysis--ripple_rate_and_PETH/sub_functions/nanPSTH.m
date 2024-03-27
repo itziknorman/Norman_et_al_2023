0ab8c92e4771532db1cb9aeae3b954c9762aa79a
itziknorman <59057794+itziknorman@users.noreply.github.com>
2024-03-27 12:14:32 -0700

    Update nanPSTH.m
    bug fix

--- analysis--ripple_rate_and_PETH/sub_functions/nanPSTH.m
+++ analysis--ripple_rate_and_PETH/sub_functions/nanPSTH.m
@@ -4,7 +4,7 @@ function  [r,binscenters] = nanPSTH(data, binsize, wsize, fs, trialmask)
 % ===============================================================
 % [r,binscenters]  = nanPSTH(data, binsize, wsize, fs, trialmask)
 % ===============================================================
-% data - events matrix (samples X trials); 1=event, 0=no-event, NaN=ignored timepoint 
+% data - events matrix (samples X trials); 1=event, 0=no-event, NaN=ignored timepoint
 % binsize - binwidth (samples)
 % wsize - size of triangular smoothing window (bins).
 % fs - sampling rate (hz)
@@ -19,27 +19,26 @@ nargoutchk(1,3);
 trialmask = []; % (!)
 if isempty(trialmask), trialmask = ones(size(data)); end
 triallen = sum(trialmask,2);
-
 % Compute PSTH:
-binsizems = binsize/fs*1000; % in msec
-lastBinms = binsizems * floor(size(data,1)./binsize);
-t =  0 : 1 : (size(data,1))/fs*1000;
+binsizems = floor(binsize/fs*1000); % in msec
+lastBinms = floor(binsizems * floor(size(data,1)./binsize));
+% t = 1/fs*1000 : 1/fs*1000 : size(data,1)/fs*1000;
+t = linspace(0, size(data,1)/fs*1000,  size(data,1));
 t = ceil(t);
 edges = 0 : binsizems : lastBinms;
-edges = ceil(edges); 
+edges = ceil(edges);
 binscenters = edges(1:end-1)+(binsizems/2);
-
 r = nan(size(binscenters));
-for k = 1:length(binscenters)   
+for k = 1:length(binscenters)
     timeind = t>=edges(k) & t<edges(k+1);
-    assert(sum(timeind)==binsize);
+    assert((sum(timeind)-binsize)<ceil(1/fs*1000));
     nSamples = nansum(nansum(trialmask(timeind,:),2));
     if nSamples == 0, continue; end % require at least 1 trial to compute PSTH
     r(k) = nansum(nansum(data(timeind,:),2)) ./ (nSamples/fs);
 end
 if nargout < 2, clear binscenters; end
 %fprintf('\n PSTH succesfully computed \n');
-r = nanfastsmooth(r,wsize,2,0.5); 
+r = nanfastsmooth(r,wsize,2,0.5);
 
 
 
